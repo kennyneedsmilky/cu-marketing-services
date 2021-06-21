d91ba5d1f056f6ae652b08498cc017b7ff10fb9d
Kenneth Fullbright (フルブライト　ケネス) <74458502+kennyneedsmilky@users.noreply.github.com>
2021-06-21 22:48:47 +0900

    small updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 /* CU Marketing Services */ "use strict"; console.log(new Date);
+
+/* ***** DOM */
+const heroImgBtn = document.querySelector(".hero-img__btn"); // Hero Image Button
+
+/* ***** Event Handlers */
+
+// Hero Image Button
+heroImgBtn.addEventListener("click", () => {
+    goToWhyChooseUs();
+})
+
+
+/* ***** Functions */
+
+// Go to "Why Choose Us".
+function goToWhyChooseUs() {
+  window.location = "#why-choose-us";  
+}
