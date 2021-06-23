fba8232b2ee8f50f53c8c7813e1ccf96f3fdfff1
Kenneth Fullbright (フルブライト　ケネス) <74458502+kennyneedsmilky@users.noreply.github.com>
2021-06-24 01:40:38 +0900

    Remade the client ratings feature.

--- assets/js/client-ratings.js
+++ assets/js/client-ratings.js
@@ -1,6 +1,8 @@
 "use strict";
-const testimonyBox = document.querySelector(".testimony-box");
+const testimonyBox = document.querySelector(".testimony-box"); // Testimony Box
+const testimonyBoxBtns = document.querySelectorAll(".testimony-box__btn"); // Testimony Btns
 
+// Client Ratings
 const clientRatings = [
     { 
         clientPic: "person-001.jpg",
@@ -13,10 +15,17 @@ const clientRatings = [
         clientName: "Roneshia B.",
         rating: 5,
         message: "Candace was very prompt and professional with setting up my website for my business, she communicated the whole way through, made sure I understood how to navigate my site & also she keeps in contact with me jsut to make sure the site is up to date if need be. I highly recommend her to my friends and family."
-    },
+    }
 ]
 
+// SLide Count
+let currentSlide = 1;
+let numberOfSlides = clientRatings.length;
+console.log(`The number of slides are ${numberOfSlides}.`);
 
+/* Event Handlers */
+
+// Generate testimonies for desktop.
 clientRatings.forEach( testimony => {
     testimonyBox.insertAdjacentHTML("beforeend", `
     <!-- Testimony -->
@@ -56,4 +65,78 @@ function generateStars (stars) {
     <i class="fas fa-star"></i>
     <i class="fas fa-star"></i>
     <i class="fas fa-star"></i>`;
+}
+
+// Last Testimony
+testimonyBox.insertAdjacentHTML("beforeend", `
+    <!-- Testimony -->
+    <div class="testimony">
+        <img class="testimony__img" src="assets/img/${ clientRatings[0].clientPic }" alt="">
+        <h2>${ clientRatings[0].clientName }</h2>
+        <p class="testimony__rating">${ generateStars( clientRatings[0].rating) }</p>
+        <p class="testimony__msg">${ clientRatings[0].message }</p>
+    </div>
+`)
+
+// Testimony Box Buttons
+testimonyBoxBtns.forEach(btn => {
+    btn.addEventListener("click", e => {
+        // Get a node list of all of the testimonies.
+        const testimonies = document.querySelectorAll(".testimony");
+
+        // If left...
+        if(e.target.classList.contains("--left")) {
+            sliderLeft(testimonies);
+        }
+
+        // If right...
+        if(e.target.classList.contains("--right")) {
+            sliderRight(testimonies);
+        }
+    })
+})
+
+setInterval(() => {
+    const testimonies = document.querySelectorAll(".testimony");
+    sliderRight(testimonies);
+}, 15000);
+
+/* Functions */
+
+// Slider Left
+function sliderLeft(testimonies) {
+    // Check to see if the first slide
+    if (currentSlide === 1) {
+        currentSlide = numberOfSlides;
+        // Go to the last slide.
+        testimonies.forEach(item => {
+            item.style.transform = `translateX(calc(-${(currentSlide - 1)}00% - ${(currentSlide - 1) * 2}rem))`;
+        })
+    } else {
+        currentSlide -= 1;
+        // Go to the previous slide.
+        testimonies.forEach(item => {
+            item.style.transform = `translateX(calc(-${(currentSlide - 1)}00% - ${(currentSlide - 1) * 2}rem))`;
+        })
+    }
+}
+
+
+// Slider Right
+function sliderRight(testimonies) {
+    // Check to see if the first slide
+    if (currentSlide === numberOfSlides) {
+        currentSlide = 1;
+        // Go to the first slide.
+        testimonies.forEach(item => {
+            item.style.transform = `translateX(calc(${(currentSlide - 1)}00% - ${(currentSlide - 1) * 2}rem))`;
+        })
+    } else {
+        currentSlide += 1;
+        // Go to the next slide.
+        testimonies.forEach(item => {
+            item.style.transform = `translateX(calc(-${(currentSlide - 1)}00% - ${(currentSlide - 1) * 2}rem))`;
+        })
+    }
+
 }
